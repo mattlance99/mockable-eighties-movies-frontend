d08433e90f09149738a4ec5d4a79ad06425a708d
github username <github email address>
2019-04-17 11:48:14 -0400

    added movies.js

--- src/containers/Movies.js
+++ src/containers/Movies.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
 
+const allMovies = [
+  { id: 1, title: "Kingdom Come", genre: "Tall tale", release_year: "1989" },
+  {
+    id: 2,
+    title: "Snowbirds Don't Fly",
+    genre: "Fairy tale",
+    release_year: "1988"
+  }
+];
+
 class Movies extends Component {
+  state = { movies: allMovies };
   render() {
-    return <h2> moveis</h2>;
+    return (
+      <div>
+        <h2> moveis</h2>
+        {this.state.movies.map(movie => (
+          <div>
+            <h3>{movie.title}</h3>
+            <h6>{movie.release_year}</h6>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
